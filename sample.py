e3fab9c253da616ee3046cac444eafe75957247b
Alondra Prado <alondra.prado@apptiva.ch>
2026-03-27 09:46:47 +0100

    exercise 1.2

--- sample.py
+++ sample.py
@@ -6,3 +6,12 @@ client = MongoClient(connection_string)
 
 # 2. Test: Informationen vom Server abrufen
 print(client.server_info()) 
+
+# Aufgabe 1.2
+# Verbindung testen 
+client.admin.command('ping')
+print("Verbindung erfolgreich: Der Server antwortet!")
+
+# Datenbanken auflisten
+dbs = client.list_database_names()
+print("Vorhandene Datenbanken:", dbs)
