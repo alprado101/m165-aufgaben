88de92c7f3637fa4ed45553513ee5f12d1f91bff
Alondra Prado <alondra.prado@apptiva.ch>
2026-04-24 09:39:34 +0200

    1. Setup - Exercise 1.1

--- software.py
+++ software.py
@@ -7,6 +7,10 @@ client = MongoClient(connection_string)
 # 2. Test: Informationen vom Server abrufen
 print(client.server_info()) 
 
+#Aufgabe 1.1
+# ODM steht für Object-Document Mapper.
+# Ein ODM ist ein Übersetzer. Er verbindet zwei Welten. ODM erlaubt,Datenbank-Einträge so zu behandeln, als wären es ganz normale Variablen im Code. So musst man keine komplizierten Datenbank-Befehle schreiben. Er übersetzt Python-Objekt in ein MongoDB-Dokument und umgekehrt.
+
 # Aufgabe 1.2
 # Verbindung testen 
 client.admin.command('ping')
